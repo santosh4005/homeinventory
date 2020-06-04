a4dbed58bdca5fd08b8b63c448b1ac326ff05716
santosh4005 <santosh.chintamaneni@gmail.com>
2020-06-04 01:09:55 -0500

    home page Safe area

--- lib/screens/myhomepage.dart
+++ lib/screens/myhomepage.dart
@@ -50,31 +50,33 @@ class _MyHomePageState extends State<MyHomePage> {
                 colors: [ Colors.purple, Colors.white],
                 begin: Alignment.topRight,
                 end: Alignment.bottomLeft)),
-        child: Padding(
-          padding: EdgeInsets.all(20.0),
-          child: GridView(
-            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 3,
-                childAspectRatio: 3 / 2,
-                crossAxisSpacing: 10.0,
-                mainAxisSpacing: 10.0),
-            children: <Widget>[
-              InkWell(
-                child: Container(
-                  color: Colors.grey,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(Icons.list),
-                      Text("My Inventory!"),
-                    ],
+        child: SafeArea(
+                  child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: GridView(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 3 / 2,
+                  crossAxisSpacing: 10.0,
+                  mainAxisSpacing: 10.0),
+              children: <Widget>[
+                InkWell(
+                  child: Container(
+                    color: Colors.grey,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Icon(Icons.list),
+                        Text("My Inventory!"),
+                      ],
+                    ),
                   ),
+                  onTap: () {
+                    Navigator.of(context).pushNamed(ScreenInventory.name);
+                  },
                 ),
-                onTap: () {
-                  Navigator.of(context).pushNamed(ScreenInventory.name);
-                },
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
